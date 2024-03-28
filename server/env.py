862c52d4fd19687897664002f44509fd4c0809cc
Mang Yau <mangjieng@gmail.com>
2024-03-28 10:20:29 -0400

    change default value of CLIENT_ID env var

--- server/env.py
+++ server/env.py
@@ -3,4 +3,4 @@ import os
 IBC_HOST = os.environ.get("IBC_HOST", "127.0.0.1")
 IBC_PORT = os.environ.get("IBC_PORT", "4002")
 IBC_CONN_TIMEOUT = int(os.environ.get("IBC_CONN_TIMEOUT", "10"))
-CLIENT_ID = int(os.environ.get("CLIENT_ID", "419"))
+CLIENT_ID = int(os.environ.get("CLIENT_ID", "123"))
